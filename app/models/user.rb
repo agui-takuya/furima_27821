5c88944d8803763b636b93b68ba5f67e21569df3
takuya agui <agut8513@gmail.com>
2020-08-23 15:00:40 +0900

    バリデーション　まとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname, presence: true, uniqueness: true
-  validates :fist_name, presence: true, uniqueness: true
-  validates :family_name, presence: true, uniqueness: true
-  validates :fist_name_kana, presence: true, uniqueness: true
-  validates :family_name_kana, presence: true, uniqueness: true
-  validates :birth_day, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+  with_options presence: true do
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :nickname, uniqueness: true
+    validates :fist_name, uniqueness: true
+    validates :family_name, uniqueness: true
+    validates :fist_name_kana, uniqueness: true
+    validates :family_name_kana, uniqueness: true
+    validates :birth_day
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_scope :user do
-  #   get 'send_destinations', to: 'users/registrations#new_send_destination'
-  #   post 'send_destinations', to: 'users/registrations#create_send_destination'
-  # end
+  
   root 'items#index'
 
   resources :users, only: [:index, :edit,:update]
