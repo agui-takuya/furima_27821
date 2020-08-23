74b1884c545ba23a4ee55cb347e70959958d3c6f
takuya agui <agut8513@gmail.com>
2020-08-23 14:31:19 +0900

    user登録設定　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true, uniqueness: true
+  validates :fist_name, presence: true, uniqueness: true
+  validates :family_name, presence: true, uniqueness: true
+  validates :fist_name_kana, presence: true, uniqueness: true
+  validates :family_name_kana, presence: true, uniqueness: true
+  validates :birth_day, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 end
