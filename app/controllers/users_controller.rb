dc5489d76eee131b1f77934c525a3592078f4e43
takuya agui <agut8513@gmail.com>
2020-08-24 16:06:29 +0900

    user登録　修正　last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,41 +7,7 @@ class UsersController < ApplicationController
     redirect_to action: :index unless user_signed_in?
   end
 
-  #   def new
-  #     @user = User.new
-  #   end
-
-  #   def create
-  #     @user = User.new(sign_up_params)
-  #     binding.pry
-  #     unless @user.valid?
-  #       flash.now[:alert] = @user.errors.full_messages
-  #       render :new and return
-  #     end
-  #     # session["devise.regist_data"] = {user: @user.attributes}
-  #     # session["devise.regist_data"][:user]["password"] = params[:user][:password]
-  #  end
-
-  #   def create_send_destination
-  #     @user = User.new(session["devise.regist_data"]["user"])
-  #     @send_destination = SendDestination.new(send_destination_params)
-  #     unless @send_destination.valid?
-  #       flash.now[:alert] = @send_destination.errors.full_messages
-  #       render :new_send_destination and return
-  #     end
-  #     @user.build_send_destination(@send_destination.attributes)
-  #     @user.save
-  #     sign_in(:user, @user)
-  #     redirect_to root_path
-  #   end
-
   private
 
-  # def user_params
-  #   params.require(:user).permit(:first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day)
-  # end
-
-  #   def send_destination_params
-  #     params.require(:send_destination).permit(:destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefecture_code, :city, :house_number, :building_name, :phone_number)
-  #   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+         :recoverable, :rememberable, :validatable  
+       
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
   with_options presence: true do
-    validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
     validates :nickname,uniqueness: true
     validates :fist_name, format: { with: /\A[ぁ-ん-龥]/, message: "is invalid. Input full-width characters."}
     validates :family_name, format: { with: /\A[ぁ-ん-龥]/, message: "is invalid. Input full-width characters."}
     validates :fist_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
-   
+  end
 end
